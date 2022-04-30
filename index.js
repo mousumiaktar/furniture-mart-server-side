9a845f2054b17d70ae3aceaf66bc0c1e853cf246
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2022-04-30 12:29:55 +0600

    get api added

--- index.js
+++ index.js
@@ -11,14 +11,31 @@ app.use(cors());
 app.use(express.json());
 
 
+// var MongoClient = require('mongodb').MongoClient;
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.g5v7d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+var uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-shard-00-00.g5v7d.mongodb.net:27017,cluster0-shard-00-01.g5v7d.mongodb.net:27017,cluster0-shard-00-02.g5v7d.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-h4324x-shard-0&authSource=admin&retryWrites=true&w=majority`;
+// MongoClient.connect(uri, function(err, client) {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.g5v7d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 
 async function run() {
     try{
         await client.connect();
         const inventoryCollection = client.db('furnitureMart').collection('inventory');
+
+        app.get('/inventory', async(req, res) => {
+            const query = {};
+            const cursor = inventoryCollection.find(query);
+            const inventories = await cursor.toArray();
+            res.send(inventories);
+        });
 
     }
     finally{
