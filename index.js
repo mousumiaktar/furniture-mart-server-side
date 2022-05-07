a29f686df6b7c7015f03b72c86232e01a7504a1f
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2022-05-08 02:21:11 +0600

    jwt completed

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 require('dotenv').config();
@@ -10,8 +11,22 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' });
+        }
+        console.log('decoded', decoded);
+        req.decoded = decoded;
+    })
+    next();
+}
 
-// var MongoClient = require('mongodb').MongoClient;
 
 var uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-shard-00-00.g5v7d.mongodb.net:27017,cluster0-shard-00-01.g5v7d.mongodb.net:27017,cluster0-shard-00-02.g5v7d.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-h4324x-shard-0&authSource=admin&retryWrites=true&w=majority`;
 
@@ -19,12 +34,24 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 
 async function run() {
-    try{
+    try {
         await client.connect();
         const inventoryCollection = client.db('furnitureMart').collection('inventory');
 
 
-        app.get('/inventory', async(req, res) => {
+        // AUTH
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({ accessToken });
+        })
+
+
+
+        // INVENTORY API
+        app.get('/inventory', async (req, res) => {
             const query = {};
             const cursor = inventoryCollection.find(query);
             const inventories = await cursor.toArray();
@@ -32,15 +59,15 @@ async function run() {
         });
 
 
-        app.get('/inventory/:id', async(req, res)=>{
+        app.get('/inventory/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const inventory = await inventoryCollection.findOne(query);
             res.send(inventory);
         })
 
 
-        app.post('/inventory', async(req, res) =>{
+        app.post('/inventory', async (req, res) => {
             const newInventory = req.body;
             console.log(newInventory);
             const result = await inventoryCollection.insertOne(newInventory);
@@ -53,48 +80,53 @@ async function run() {
             const updatedStock = req.body;
             const filter = { _id: ObjectId(id) };
             const options = { upsert: true };
-        
+
             const updateDoc = {
-              $set: {
-                quantity: updatedStock.quantity,
-              },
+                $set: {
+                    quantity: updatedStock.quantity,
+                },
             };
             const result = await inventoryCollection.updateOne(filter, updateDoc, options);
             res.send(result);
-          });
+        });
 
 
 
-        app.delete('/inventory/:id', async(req, res) =>{
+        app.delete('/inventory/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const result = await inventoryCollection.deleteOne(query);
             res.send(result);
         })
 
 
-        app.get('/inventories', async(req, res) =>{
+        app.get('/inventories', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
             const email = req.query.email;
-            console.log(email);
-            const query = {email: email};
-            const cursor = inventoryCollection.find(query);
-            const inventories = await cursor.toArray();
-            res.send(inventories);
+            if (email === decodedEmail) {
+                const query = { email: email };
+                const cursor = inventoryCollection.find(query);
+                const inventories = await cursor.toArray();
+                res.send(inventories);
+            }
+            else{
+                res.status(403).send({message: 'forbidden access'})
+            }
         })
 
 
     }
-    finally{
+    finally {
 
     }
 }
 run().catch(console.dir);
 
 
-app.get('/',(req, res)=>{
+app.get('/', (req, res) => {
     res.send('Server is Running');
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log('listening to port', port);
 });
